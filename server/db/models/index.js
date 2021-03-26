afe3a61a2dbe23f2e6477511c36d242b7248c7be
Jessica Lee <jessicalee546@gmail.com>
2021-03-26 12:07:34 -0400

    added new associations

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,10 +1,12 @@
 const User = require('./user')
 const Card = require('./card')
 
-
 //User has a one-to-many association with Card
-User.hasMany(Card)
+User.hasMany(Card, {foreignKey: 'creatorId'})
 Card.belongsTo(User)
+
+Card.hasMany(User, {foreignKey: 'recipientId'})
+User.belongsTo(Card)
 
 module.exports = {
   User,
