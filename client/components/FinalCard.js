b453c1746e820e7a72f20c4587dec42c9f8e2126
Ari Kamarchevakul <snowbearypie@gmail.com>
2021-03-29 17:00:49 -0500

    feat: Added Envelope to Final Card View

--- client/components/FinalCard.js
+++ client/components/FinalCard.js
@@ -1,17 +1,24 @@
 import React from 'react'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 import {connect} from 'react-redux'
 import {addUrl, sendEmail} from '../store/card'
 import Button from 'react-bootstrap/Button'
 import history from '../history'
+import {Motion, spring} from 'react-motion';
+
 
 class FinalCard extends React.Component {
   constructor(props) {
     super(props)
     this.canvas = React.createRef()
-    this.state = {canvasUrl: {Url: ''}}
+    this.state = {canvasUrl: {Url: ''}, open: false}
     this.onClick = this.onClick.bind(this)
+    this.handleMouseDown = this.handleMouseDown.bind(this)
   }
+
+  handleMouseDown = () => {
+    this.setState({open: !this.state.open});
+  };
 
   componentDidMount() {
     let canvas = this.canvas.current
@@ -44,7 +51,7 @@ class FinalCard extends React.Component {
     ctx.font = '20px Sans Serif'
 
     img.onload = () => {
-      ctx.drawImage(img, 0, 0, 600, 400)
+      ctx.drawImage(img, 0, 0, 600, 434.11)
       var lines = text.split('\n')
       for (var j = 0; j < lines.length; j++) {
         ctx.fillText(lines[j], x, y + j * 25)
@@ -73,13 +80,29 @@ class FinalCard extends React.Component {
 
   render() {
     return (
-      <Jumbotron id="final-card">
-        <canvas ref={this.canvas} width="600" height="400" />
+      <Container id="final-card">
+        <Motion style={{x: spring(this.state.open ? -650 : 0)}}>
+          {({x}) =>
+            <>
+              <canvas ref={this.canvas} width="600" height='434.11' />
+              <div
+                onMouseDown={this.handleMouseDown}
+                onTouchStart={this.handleTouchStart}
+                className="envelope" style={{
+                WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                transform: `translate3d(${x}px, 0, 0)`,
+              }}>
+                <div className='envelope-text'>A card from {this.props.card.card.name}<br />Click me to open</div>
+              </div>
+            </>
+          }
+        </Motion>
         <br />
-        <Button variant="dark" onClick={this.onClick}>
+        <Button className='final-card-button' variant="dark" onClick={this.onClick}>
           Send my card!
         </Button>
         <Button
+          className='final-card-button'
           variant="dark"
           onClick={() => {
             history.push('/')
@@ -87,7 +110,7 @@ class FinalCard extends React.Component {
         >
           Start over
         </Button>
-      </Jumbotron>
+      </Container>
     )
   }
 }
