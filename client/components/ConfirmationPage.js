22fced53a916936fd00c9686d65f6cb861d90c2f
Jessica Lee <jessicalee546@gmail.com>
2021-03-31 17:55:15 -0400

    fix: updated confirmation page

--- client/components/ConfirmationPage.js
+++ client/components/ConfirmationPage.js
@@ -2,21 +2,35 @@ import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Button from 'react-bootstrap/Button'
 import {Link} from 'react-router-dom'
-
+import {connect} from 'react-redux'
 
 class ConfirmationPage extends React.Component {
   render() {
     return (
       <Jumbotron id="confirmation-page">
-        <h2>Your card has been sent!</h2>
-        <Link to="/home">
-          <Button variant="dark">
-            Make another!
-          </Button>
-        </Link>
+        <h2 className="centered-header">Your card has been sent!</h2>
+        <div className="button-container">
+          <Link to="/home">
+            <Button variant="dark">Make another!</Button>
+          </Link>
+        </div>
+        {this.props.isLoggedIn ? (
+          <p className="centered-header">
+            <Link to="/user/cards">Click here</Link> to see your cards.
+          </p>
+        ) : (
+          <p className="centered-header">
+            <Link to="/signup">Sign up</Link> to save your cards.
+          </p>
+        )}
       </Jumbotron>
     )
   }
 }
+const mapState = state => {
+  return {
+    isLoggedIn: !!state.user.id
+  }
+}
 
-export default ConfirmationPage
+export default connect(mapState)(ConfirmationPage)
